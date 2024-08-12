812f23d8473a3ed19183bee6dd47728111b049ce
Subham0003 <subham0003@gmail.com>
2024-08-12 15:48:29 +0530

    commit with new project of RAG with GROQ

--- ollama/app.py
+++ ollama/app.py
@@ -8,6 +8,9 @@ import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+
+
+
 ## Langsmith Tracking
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"]="true"
