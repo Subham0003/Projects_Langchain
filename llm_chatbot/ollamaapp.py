3753350a920eaf14388a455be782ee0a7ae8e96d
Subham Patel <135098678+Subham0003@users.noreply.github.com>
2024-08-25 00:15:04 +0530

    Update ollamaapp.py

--- llm_chatbot/ollamaapp.py
+++ llm_chatbot/ollamaapp.py
@@ -6,7 +6,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.llms import Ollama
 from langchain_openai import ChatOpenAI
-
+###update
 
 #creating chatprompt
 
