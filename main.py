ee40ea2ed466eedb0659d3021204a509c5868c6f
Subham Patel <135098678+Subham0003@users.noreply.github.com>
2024-08-25 00:25:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import load_model
-
+##update
 # Load the IMDB dataset word index
 word_index = imdb.get_word_index()
 reverse_word_index = {value: key for key, value in word_index.items()}
